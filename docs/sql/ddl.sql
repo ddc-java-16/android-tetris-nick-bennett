520ba93e6ccecbdf5a29b9b6651fdff364b61bdd
nick-bennett <nbennett10@cnm.edu>
2023-10-16 17:18:01 -0600

    Refreshed generated DDL.

--- docs/sql/ddl.sql
+++ docs/sql/ddl.sql
@@ -1,4 +1,4 @@
--- Generated 2023-07-26 16:15:04-0600 for database version 1
+-- Generated 2023-10-16 15:45:24-0600 for database version 1
 
 CREATE TABLE IF NOT EXISTS `user`
 (
@@ -11,3 +11,17 @@ CREATE TABLE IF NOT EXISTS `user`
 CREATE UNIQUE INDEX IF NOT EXISTS `index_user_oauth_key` ON `user` (`oauth_key`);
 
 CREATE UNIQUE INDEX IF NOT EXISTS `index_user_display_name` ON `user` (`display_name`);
+
+CREATE TABLE IF NOT EXISTS `score`
+(
+    `score_id`     INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    `created`      INTEGER                           NOT NULL,
+    `started`      INTEGER                           NOT NULL,
+    `duration`     INTEGER                           NOT NULL,
+    `value`        INTEGER                           NOT NULL,
+    `rows_removed` INTEGER                           NOT NULL,
+    `player_id`    INTEGER                           NOT NULL,
+    FOREIGN KEY (`player_id`) REFERENCES `user` (`user_id`) ON UPDATE NO ACTION ON DELETE CASCADE
+);
+
+CREATE INDEX IF NOT EXISTS `index_score_player_id` ON `score` (`player_id`);
